40822e47b23eb5f524106d0ada290312237f34a4
Sam Kavanagh <hganavak@gmail.com>
2019-09-13 11:39:24 +1200

    Major update: V4 (supports day of the week mode)

--- weather.sh
+++ weather.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-# Weather-Wallpaper V2: Sam Kavanagh, Tofu.dev.
+# Weather-Wallpaper V4: Sam Kavanagh, Tofu.dev.
 # See the README for setup instructions.
 
 #==========================================#
@@ -8,7 +8,7 @@
 #==========================================#
 
 # Path to weather wallpapers directory
-WEATHER_WALLPAPERS_DIRECTORY="/home/sam/Pictures/weather-wallpapers"
+WEATHER_WALLPAPERS_DIRECTORY="/home/sam/Pictures/weather-wallpapers/"
 
 # Wallpaper filenames and their corresponding weather conditions
 HEAVY_RAIN_WALLPAPER="Rain.jpg" # Rain, Heavy Rain, Thunderstorm
@@ -17,9 +17,45 @@ CLOUDY_WALLPAPER="Cloudy.jpg" # Cloudy, Overcast, Mist, Fog
 CLEAR_WALLPAPER="Clear.jpg" # Clear
 SUNNY_WALLPAPER="Sunny.jpg" # Sunny
 SNOW_WALLPAPER="Snow.jpg" # Snow
+FALLBACK_WALLPAPER="Fallback.jpg" # Fallback: In case of no Internet access or other error
+
+DOW_MODE_ENABLED=false # Selects the pictures from a different folder depending on the day of the week
 
 #==========================================#
 #             END USER SETTINGS            #
+#==========================================#
+
+# Get the DBUS_SESSION_BUS_ADDRESS environment variable: gsettings needs this but cron by default runs without it
+PID=$(pgrep gnome-session | tail -n1)
+export DBUS_SESSION_BUS_ADDRESS=$(grep -z DBUS_SESSION_BUS_ADDRESS /proc/$PID/environ|cut -d= -f2-)
+export GIO_EXTRA_MODULES=/usr/lib/x86_64-linux-gnu/gio/modules/ # Allows you to run the script not as CRON
+
+#==========================================#
+#         DAY OF THE WEEK SETTINGS         #
+#==========================================#
+
+if [ "$DOW_MODE_ENABLED" = true ] ; then
+	DOW=$(date +%u)
+	case $DOW in
+		1) WEATHER_WALLPAPERS_DIRECTORY="/home/sam/Pictures/weather-wallpapers/monet/" # Monday: Monet
+			;;
+		2) WEATHER_WALLPAPERS_DIRECTORY="/home/sam/Pictures/weather-wallpapers/matisse/" # Tuesday: Matisse
+			;;
+		3) WEATHER_WALLPAPERS_DIRECTORY="/home/sam/Pictures/weather-wallpapers/friedrich/" # Wednesday: Friedrich
+			;;
+		4) WEATHER_WALLPAPERS_DIRECTORY="/home/sam/Pictures/weather-wallpapers/chagall/" # Thursday: Chagall
+			;;
+		*) RPIC=$(find /home/sam/Pictures/weather-wallpapers/ -not -type d | shuf -n 1); # Fridays & Weekends: Randomized picture
+			echo "$RPIC"
+			gsettings set org.gnome.desktop.background picture-uri "file://${RPIC}"
+			gsettings set org.gnome.desktop.screensaver picture-uri "file://${RPIC}"
+			exit;
+			;;
+	esac
+fi
+
+#==========================================#
+#       END DAY OF THE WEEK SETTINGS       #
 #==========================================#
 
 # Welcome message 
@@ -48,14 +84,14 @@ elif [[ $CUR_WEATHER =~ Sunny ]]; then
 	SELECTED_WALLPAPER=$SUNNY_WALLPAPER;
 elif [[ $CUR_WEATHER =~ Snow ]]; then
 	SELECTED_WALLPAPER=$SNOW_WALLPAPER;
+else
+	SELECTED_WALLPAPER=$FALLBACK_WALLPAPER;
 fi
 
 echo -e "Setting wallpaper to:\t $SELECTED_WALLPAPER"
 
-# Get the DBUS_SESSION_BUS_ADDRESS environment variable: gsettings needs this but cron by default runs without it
-PID=$(pgrep gnome-session | tail -n1)
-export DBUS_SESSION_BUS_ADDRESS=$(grep -z DBUS_SESSION_BUS_ADDRESS /proc/$PID/environ|cut -d= -f2-)
 
 # Set the wallpaper
 echo -e "file://${WEATHER_WALLPAPERS_DIRECTORY}/${SELECTED_WALLPAPER}"
 gsettings set org.gnome.desktop.background picture-uri "file://${WEATHER_WALLPAPERS_DIRECTORY}/${SELECTED_WALLPAPER}"
+gsettings set org.gnome.desktop.screensaver picture-uri "file://${WEATHER_WALLPAPERS_DIRECTORY}/${SELECTED_WALLPAPER}"
